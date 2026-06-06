22299d36fb10c35f98dbb82f82b4c218c3d58951
Benze <s.buachom@gmail.com>
2026-06-06 04:49:02 +0000

    Write my frist program in Python

---
+++ my_frist_program.py
@@ -0,0 +1,31 @@
+# print("Hello world!")
+# print("data pipeline")
+# naem = "Benze"
+# number = 10
+# a = 5.5
+# b = 9
+# print(naem, number,a,b)
+
+# c=a+b
+# c=c*10
+# print(c)
+
+# print(5%4)
+# print(5%2)
+
+
+# if c>150:
+#     c=100
+#     a=100
+#     b=100
+# else:
+#     c=9
+#     a=9
+#     b=9
+
+# print(a,b,c)
+
+l = [1,2,3,4,5,6]
+for each_item in l:
+    if each_item % 2==0:
+        print(each_item)
